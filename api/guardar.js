6c2ab951df6408a4aa5d5cecbde96d2aa6965521
pabloesteban3012-ops <pabloesteban3012@gmail.com>
2026-03-30 08:41:57 -0500

    Update guardar.js

--- api/guardar.js
+++ api/guardar.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  'https://xqjizdzginycrwsiwmpk.supabase.co/',
+  process.env.SUPABASE_URL,      
   process.env.SUPABASE_ANON_KEY
 );
 
