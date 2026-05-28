61d6a287d6f17e896ca76cea8773171aa57c0981
pabloesteban3012-ops <pabloesteban3012@gmail.com>
2026-05-28 14:25:05 -0500

    Create me.js

---
+++ api/me.js
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+  const cookieHeader = req.headers.cookie || '';
+  const cookies = Object.fromEntries(
+    cookieHeader
+      .split(';')
+      .map(v => v.trim())
+      .filter(Boolean)
+      .map(v => v.split('='))
+  );
+
+  if (!cookies.session) {
+    return res.status(401).json({ authenticated: false });
+  }
+
+  let session;
+  try {
+    session = JSON.parse(Buffer.from(cookies.session, 'base64').toString('utf8'));
+  } catch {
+    return res.status(401).json({ authenticated: false });
+  }
+
+  return res.status(200).json({
+    authenticated: true,
+    user: session.username
+  });
+}
